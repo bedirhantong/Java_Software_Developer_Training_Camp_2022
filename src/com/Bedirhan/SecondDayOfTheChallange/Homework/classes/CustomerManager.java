07120a18d7cdcd4fca3995d2da055e3defa3e9a3
cengb <cengbedirhantong@gmail.com>
2022-09-28 19:19:05 +0300

    CustomerManager.java and Main.java classes and object created for body of hw

---
+++ src/com/Bedirhan/SecondDayOfTheChallange/Homework/classes/CustomerManager.java
@@ -0,0 +1,13 @@
+package com.Bedirhan.SecondDayOfTheChallange.Homework.classes;
+
+public class CustomerManager {
+    public void add(){
+        System.out.println("added");
+    }
+    public void update(){
+        System.out.println("updated");
+    }
+    public void remove(){
+        System.out.println("removed");
+    }
+}
---
+++ src/com/Bedirhan/SecondDayOfTheChallange/Homework/classes/Main.java
@@ -0,0 +1,11 @@
+package com.Bedirhan.SecondDayOfTheChallange.Homework.classes;
+
+public class Main {
+    public static void main(String[] args) {
+        CustomerManager customerManager = new CustomerManager();
+        customerManager.add();
+        customerManager.remove();
+        customerManager.update();
+    }
+
+}
