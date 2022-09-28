a0af26811bb5b93d6c7f7af829a38433fe9f53e7
cengb <cengbedirhantong@gmail.com>
2022-09-28 19:48:12 +0300

    basic calculator object and class created

---
+++ src/com/Bedirhan/SecondDayOfTheChallange/Homework/classes/reCapDemo_Classes/Main.java
@@ -0,0 +1,11 @@
+package com.Bedirhan.SecondDayOfTheChallange.Homework.classes.reCapDemo_Classes;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        System.out.println(calculator.add(4,7));
+        System.out.println(calculator.divide(10,4));
+        System.out.println(calculator.multiply(4,2));
+        System.out.println(calculator.subtract(43,2));
+    }
+}
