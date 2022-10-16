521ac592f75888e37710eef325ec5d1d7a2abba7
cengb <cengbedirhantong@gmail.com>
2022-10-16 12:21:09 +0300

    Overloaded indexOf method explained with usage of its second parameter

--- src/com/Bedirhan/ThirdDayOfTheChallenge/Homeworks/Odev2/StringClassesMethods/KarakterMetodlari.java
+++ src/com/Bedirhan/ThirdDayOfTheChallenge/Homeworks/Odev2/StringClassesMethods/KarakterMetodlari.java
@@ -7,18 +7,18 @@ public class KarakterMetodlari {
         char[] kaynak2 = new char[9];
         char[] kaynak3 = new char[11];
         dizge.getChars(11,17,kaynak1,0);
-        System.out.println(kaynak1);
-        dizge.getChars(18,27,kaynak2,0);
-        System.out.println(kaynak2);
-        dizge.getChars(41,52,kaynak3,0);
-        System.out.println(kaynak3);
+//        System.out.println(kaynak1);
+//        dizge.getChars(18,27,kaynak2,0);
+//        System.out.println(kaynak2);
+//        dizge.getChars(41,52,kaynak3,0);
+//        System.out.println(kaynak3);
 
 
 
 
 
         String str  = "abcdefghıjklmnoprstuvyz";
-        System.out.println(str.charAt(2));
+//        System.out.println(str.charAt(2));
 
         int uzunluk = str.length();
         char[] arr =  new char[uzunluk];
@@ -27,6 +27,8 @@ public class KarakterMetodlari {
         System.out.println(str.indexOf("yz"));
         System.out.println(str.lastIndexOf("a"));
         System.out.println(str.indexOf("ıjk"));
+        System.out.println(str.indexOf("ş"));
+        System.out.println(str.indexOf("a",12));
 
 
 
