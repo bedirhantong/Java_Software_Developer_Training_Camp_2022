093fc72b98eaa048af1cc448a5880b3b15bb8fde
cengb <cengbedirhantong@gmail.com>
2022-10-16 13:29:06 +0300

    split method explained with usage with its different usages

--- src/com/Bedirhan/ThirdDayOfTheChallenge/Homeworks/Odev2/StringClassesMethods/KarakterMetodlari.java
+++ src/com/Bedirhan/ThirdDayOfTheChallenge/Homeworks/Odev2/StringClassesMethods/KarakterMetodlari.java
@@ -24,14 +24,40 @@ public class KarakterMetodlari {
         char[] arr =  new char[uzunluk];
         arr = str.toCharArray();
 
-        System.out.println(str.indexOf("yz"));
-        System.out.println(str.lastIndexOf("a"));
-        System.out.println(str.indexOf("ıjk"));
-        System.out.println(str.indexOf("ş"));
-        System.out.println(str.indexOf("a",12));
+//        System.out.println(str.indexOf("yz"));
+//        System.out.println(str.lastIndexOf("a"));
+//        System.out.println(str.indexOf("ıjk"));
+//        System.out.println(str.indexOf("ş"));
+//        System.out.println(str.indexOf("a",12));
+        String str1  = "abcdefghıjklmnoprstuvyz";
+        String chosenPart = str1.substring(1,4);
+//        System.out.println(chosenPart);
+//        System.out.println(str1.substring(18));
 
 
 
+        String str3 = "C++ Programming";
+
+        // all occurrences of "C++" is replaced with "Java"
+//        System.out.println(str3.replace("C++", "Java"));  // Java Programming
+
+
+        // all occurences of "aa" is replaced with "zz"
+//        System.out.println("aa bb aa zz".replace("aa", "zz")); // zz bb zz zz
+
+        // substring not in the string
+//        System.out.println("Java".replace("C++", "C")); // Java
+
+//        System.out.println(str3.replaceAll("r","m"));
+
+        String name = " ben sen ve biz ";
+//        System.out.println(name.trim());
+        // başındaki ve sonundaki boşluklar silindi ama string immutable sadece string döndürdük
+
+        String[] arr2 = name.split("\\s");
+        for (String val:arr2) {
+            System.out.print(val);
+        }
 
 
 
