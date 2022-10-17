c2628c55524987da22ac076188251a43136bce94
cengb <cengbedirhantong@gmail.com>
2022-10-17 22:35:33 +0300

    try catch explained with a basic example

---
+++ src/com/Bedirhan/FourthDayOfTheChallenge/Homeworks/ExceptionHandling/TryCatchDemo.java
@@ -0,0 +1,21 @@
+package com.Bedirhan.FourthDayOfTheChallenge.Homeworks.ExceptionHandling;
+
+public class TryCatchDemo {
+    public static void main(String[] args) {
+//        int a =19;
+//        int b = 0;
+//        int c;
+//        try{
+//            c = a/b;
+//        }
+//        catch (ArithmeticException e){  // exception türü
+//            System.out.println("hata: "+e.toString());
+//        }
+
+
+
+
+    }
+
+
+}
